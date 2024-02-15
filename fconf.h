5ee918f72caf03c94969a286680ce298290662cc
slavonchik7 <slavonchik0701@gmail.com>
2024-02-15 19:04:12 +0300

    added private field '__prvt' for internal using

--- fconf.h
+++ fconf.h
@@ -7,6 +7,9 @@
 
 #define __user_api
 
+#define FC_MAX_ATTR_VALUE_LEN   512
+#define FC_MAX_ATTR_LEN         128
+
 struct fc_result {
 #define FCONF_OK    0x00
 #define FCONF_ERR   0x01
@@ -15,14 +18,16 @@ struct fc_result {
 /* string msg about last error */
     char *errmsg;
     int errsize;
+
+/* for internal work, dont touch!! */
+    void *__prvt;
 };
 
+#define FCISERR(fcr) ((fcr)->status != FCONF_OK)
 
-#define FCISERR(fcr) ((fcr)->status == FCONF_ERR)
 
-
-struct fc_result  __user_api *fconf_read(const char *file);
-void              __user_api fconf_clear(struct fc_result *fcr);
+extern struct fc_result  __user_api *fconf_read(const char *file);
+extern void              __user_api fconf_clear(struct fc_result *fcr);
 
 #undef __user_api
 #endif /* __FCONF_H__ */
